63e9e12b3519af47cbfee7e801a37b391a5cc2fd
Max <mm4ate@gmail.com>
2024-05-29 10:47:14 +0200

    Add custom classes

---
+++ src/main/java/com/todense/view/ComparisonView.java
@@ -0,0 +1,11 @@
+package com.todense.view;
+
+import com.todense.viewmodel.ComparisonViewModel;
+import de.saxsys.mvvmfx.FxmlView;
+
+/*
+ * View for the Comparison.
+ */
+
+public class ComparisonView implements FxmlView<ComparisonViewModel> {
+}
---
+++ src/main/java/com/todense/viewmodel/ComparisonViewModel.java
@@ -0,0 +1,10 @@
+package com.todense.viewmodel;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+/*
+ * ViewModel for the Comparison.
+ */
+
+public class ComparisonViewModel implements ViewModel {
+}
