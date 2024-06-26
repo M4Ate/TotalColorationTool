bf5af943e05100b8b233a7390174c9fd2b8d4ecf
Maximilian Paul <maximilianpaul1811@gmail.com>
2024-06-26 10:42:54 +0200

    Add left side button and change solverView as well as the SolverViewModel.

--- src/main/java/com/todense/view/SolverView.java
+++ src/main/java/com/todense/view/SolverView.java
@@ -2,10 +2,21 @@ package com.todense.view;
 
 import com.todense.viewmodel.SolverViewModel;
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 /*
  * View for the ILP Solver.
  */
 
 public class SolverView implements FxmlView<SolverViewModel> {
+
+
+    @InjectViewModel
+    SolverViewModel viewModel;
+
+    public void initialize(){
+
+    }
 }
