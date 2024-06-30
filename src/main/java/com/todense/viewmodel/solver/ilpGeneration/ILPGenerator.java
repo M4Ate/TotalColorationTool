468c5c2ac7cce8e4def4e9cbd7e6195f194b357f
Manuel Bürger <buerger-manuel@gmx.de>
2024-06-30 15:41:00 +0200

    Add Code and Javadoc for ILPType and base structure of the ILPGenerator class

--- src/main/java/com/todense/viewmodel/solver/ilpGeneration/ILPGenerator.java
+++ src/main/java/com/todense/viewmodel/solver/ilpGeneration/ILPGenerator.java
@@ -1,4 +1,55 @@
 package com.todense.viewmodel.solver.ilpGeneration;
 
+import com.todense.model.graph.Graph;
+
+/**
+ * ILPGenetrator offers a static function that creates an ILP-problem for a total coloring problem
+ */
 public class ILPGenerator {
+
+    /**
+     * creates an ILP-Problem of the specified ILPType on the basis of a given Graph
+     *
+     * @param graph graph to convert into ILP-Problem
+     * @param type type of ILP-Problem
+     * @return complete ILPProblem class
+     */
+    public static ILPProblem generateILP(Graph graph, ILPType type) {
+        ILPProblem result = null;
+        switch (type) {
+            case MINCOLORS:
+                result = minColors(graph);
+                break;
+            case SIMILARCOLORS:
+                result = similarColors(graph);
+                break;
+            case WITHSETCOLORS:
+                result = withSetColors(graph);
+                break;
+            case WITHLOWCOLORS:
+                result = withLowColors(graph);
+                break;
+        }
+        return result;
+    }
+
+    private static ILPProblem minColors(Graph graph) {
+        //To be Implemented
+        return null;
+    }
+
+    private static ILPProblem similarColors(Graph graph) {
+        //To be Implemented
+        return null;
+    }
+
+    private static ILPProblem withSetColors(Graph graph) {
+        //To be Implemented
+        return null;
+    }
+
+    private static ILPProblem withLowColors(Graph graph) {
+        //To be Implemented
+        return null;
+    }
 }
--- src/main/java/com/todense/viewmodel/solver/ilpGeneration/ILPType.java
+++ src/main/java/com/todense/viewmodel/solver/ilpGeneration/ILPType.java
@@ -1,4 +1,26 @@
 package com.todense.viewmodel.solver.ilpGeneration;
 
+/**
+ * specifies the type of ILP-Problem
+ */
 public enum ILPType {
+    /**
+     * MINCOLORS just minimizes the total amount of colors used in the total coloring ILP-Problem
+     */
+    MINCOLORS,
+    /**
+     * SIMILARCOLORS trys to use as many predefined colors as possible in the total coloring ILP-Problem
+     */
+    SIMILARCOLORS,
+    /**
+     * WITHSETCOLORS will definitely use the predefined colors in the total coloring ILP-Problem
+     */
+    WITHSETCOLORS,
+    /**
+     * WITHLOWCOLORS will try to maximize the use of low number colors in the total coloring ILP-Problem
+     */
+    WITHLOWCOLORS,
+
+
+
 }
