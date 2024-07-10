4c48f3a7d325edb6f4c282f46bdbf625692b18bf
LtMarseille <philipp_berghoff@yahoo.com>
2024-07-10 09:32:02 +0200

    refactor the edge selection process
    Using a private method to get the edges
    Problems.
       1 Edge already in graph
       2 Endless loop in case no edge is fitting

--- src/main/java/com/todense/viewmodel/random/generators/SimilarGenerator.java
+++ src/main/java/com/todense/viewmodel/random/generators/SimilarGenerator.java
@@ -22,30 +22,31 @@ public class SimilarGenerator extends RandomEdgeGenerator{
     @Override
     protected void generate() {
         //get a random node
-        int randomEdgeId = super.rnd.nextInt(this.currentGraph.getEdges().size());
-        Edge edgeOne = currentGraph.getEdges().get(randomEdgeId);
 
-        //get a second random node with different end nodes
-        Edge edgeTwo;
-        do {
-            randomEdgeId = super.rnd.nextInt(this.currentGraph.getEdges().size());
-            edgeTwo = currentGraph.getEdges().get(randomEdgeId);
-        } while (edgeOne.getN1().getIndex() == edgeTwo.getN2().getIndex()
-                | edgeTwo.getN1().getIndex() == edgeOne.getN2().getIndex());
+        Edge[] edges = getRandomEdges();
+        Edge edgeOne = edges[0];
+        Edge edgeTwo = edges[1];
 
+
+        
         Color greyColor = Color.rgb(105, 105, 105);
 
         currentGraph.removeEdge(edgeOne);
         currentGraph.removeEdge(edgeTwo);
 
         System.out.println("add edge from " + edgeOne.getN1() + " to " + edgeTwo.getN2());
+
         currentGraph.addEdge(edgeOne.getN1(), edgeTwo.getN2(), greyColor);
 
         System.out.println("add edge from " + edgeTwo.getN1() + " to " + edgeOne.getN2());
         currentGraph.addEdge(edgeTwo.getN1(), edgeOne.getN2(), greyColor);
+
+
         //validate if a Graph is already loaded
         System.out.println("generate called");
+
     }
+
 
     private boolean nodesDifferent (Edge edgeOne, Edge edgeTwo){
         if(edgeOne.getN1().getIndex() != edgeTwo.getN1().getIndex()
@@ -53,6 +54,26 @@ public class SimilarGenerator extends RandomEdgeGenerator{
         return true;
     }
 
+    private Edge[] getRandomEdges() {
+
+        Edge[] edges = new Edge[2];
+        
+        int randomEdgeId = super.rnd.nextInt(this.currentGraph.getEdges().size());
+        edges[0] = currentGraph.getEdges().get(randomEdgeId);
+
+        System.out.println("selected " + edges[0].getN1() + " and " + edges[0].getN2());
+
+
+        do {
+            randomEdgeId = super.rnd.nextInt(this.currentGraph.getEdges().size());
+            edges[1] = currentGraph.getEdges().get(randomEdgeId);
+        } while (edges[0].getN1().getIndex() == edges[1].getN2().getIndex()
+                | edges[1].getN1().getIndex() == edges[0].getN2().getIndex()
+                | edges[0].getId().equals(edges[1].getId()));
+
+        System.out.println("selected " + edges[1].getN1() + " and " + edges[1].getN2());
+        return edges;
+    }
 }
 
 
