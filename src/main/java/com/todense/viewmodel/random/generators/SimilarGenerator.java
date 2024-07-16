034115343425fdc405e1480b7d222a722f6a1cfb
LtMarseille <philipp_berghoff@yahoo.com>
2024-07-16 09:32:39 +0200

    Add coments and remove the change of color

--- src/main/java/com/todense/viewmodel/random/generators/SimilarGenerator.java
+++ src/main/java/com/todense/viewmodel/random/generators/SimilarGenerator.java
@@ -4,13 +4,12 @@ import com.todense.model.graph.Edge;
 import com.todense.model.graph.Node;
 import com.todense.model.graph.Graph;
 import com.todense.viewmodel.random.RandomEdgeGenerator;
-
-import com.todense.viewmodel.comparison.CompareLogic;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 /**
+ * @author Philipp Berghoff
  * Generates a graph similar to the already loaded one.
  * TODO add a definition what a similar Graph is
  * TODO add a java doc
@@ -25,8 +24,15 @@ public class SimilarGenerator extends RandomEdgeGenerator{
         super.setNodes(currentGraph.getNodes());
     }
 
+    /**
+     * @author Philipp Berghoff
+     * TODO add java doc
+     *
+     * @throws IllegalStateException
+     */
+
     @Override
-    protected void generate() {
+    protected void generate() throws IllegalStateException {
         //get one random nodes
 
         Edge edgeOne;
@@ -67,31 +73,36 @@ public class SimilarGenerator extends RandomEdgeGenerator{
             }
         }
 
-        //TODO if the for loop dose not finds a fitting new edge an Error should be printed
+        //in case no fitting Edge is found the exception is thrown
+        throw new IllegalStateException("none isomorphic similar Graph");
     }
 
+    //this method adds two new Edges between the first two Nodes and the second two Nodes
     private void addEdges(Node node, Node node1, Node node2, Node node3) {
-
+        //TODO delete
         System.out.println("Edge One: " + node.getID() + " -> " + node1.getID()
                 + " EdgeTwo: " + node2.getID() + " -> " + node3.getID());
 
         super.addEdge(node.getID(), node1.getID());
         super.addEdge(node2.getID(), node3.getID());
 
-        this.currentGraph.addEdge(node, node1, CompareLogic.GREY_COLOR);
+        this.currentGraph.addEdge(node, node1);
+        //TODO delete
         printNeighbors(node);
         printNeighbors(node1);
-        this.currentGraph.addEdge(node2, node3, CompareLogic.GREY_COLOR);
+        this.currentGraph.addEdge(node2, node3);
+        //TODO delete
         printNeighbors(node2);
         printNeighbors(node3);
     }
 
+    //this method returns if 2 Edges have one or more "end" nodes in common
     private boolean edgesAreDifferent (Edge edgeOne, Edge edgeTwo) {
         return (edgeOne.getN1() != edgeTwo.getN1() && edgeOne.getN2() != edgeTwo.getN2()
                 && edgeOne.getN1() != edgeTwo.getN2() && edgeOne.getN2() != edgeTwo.getN1());
     }
 
-
+    //TODO delete
     private void printNeighbors(Node node) {
         System.out.print("Neighbours of " + node.getID() + ": ");
         for (Node neighbour : node.getNeighbours()) {
