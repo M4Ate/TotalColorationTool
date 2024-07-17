b64864b419f1a956512d7c181592a32cd098d48e
LtMarseille <philipp_berghoff@yahoo.com>
2024-07-17 19:20:07 +0200

    Add Java doc for the MaxDegGenerator

--- src/main/java/com/todense/viewmodel/random/generators/MaxDegGenerator.java
+++ src/main/java/com/todense/viewmodel/random/generators/MaxDegGenerator.java
@@ -1,15 +1,31 @@
 package com.todense.viewmodel.random.generators;
-
 import com.todense.viewmodel.random.RandomEdgeGenerator;
+
+/**
+ * This class is the MaxDegGenerator. It is a child of the RandomEdgeGenerator Class.
+ * Its purpose is to distribute the edges around in a Graph.
+ * The target is that every Node has as many adjacent edges as possible or as wished form the user.
+ */
 
 public class MaxDegGenerator extends RandomEdgeGenerator {
 
     private final int maxDegree;
 
+    /**
+     * This is the constructor of the maxDegree Class.
+     * It stores the give parameter and calls the super constructor.
+     *
+     * @param maxDegree the target value for the generate method.
+     */
     public MaxDegGenerator(int maxDegree){
         super();
         this.maxDegree = maxDegree;
     }
+
+    /**
+     * This method generates the edges for the new graph.
+     * It uses the attribute set by the constructor.
+     */
 
     @Override
     protected void generate() {
