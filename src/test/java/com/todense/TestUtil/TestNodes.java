20205d9b51de9f22ddcce1c9e827de03fbb36213
LtMarseille <philipp_berghoff@yahoo.com>
2024-07-31 15:10:34 +0200

    Add the class Test nodes which is easyer to use for testing
    Add a chile of the Node class that can create Nodes with public calls

---
+++ src/test/java/com/todense/TestUtil/TestNodes.java
@@ -0,0 +1,11 @@
+package com.todense.TestUtil;
+
+import com.todense.model.graph.Node;
+import javafx.geometry.Point2D;
+
+public class TestNodes extends Node {
+
+    public TestNodes(Point2D pos, int index, int id) {
+        super(pos, index, id);
+    }
+}
