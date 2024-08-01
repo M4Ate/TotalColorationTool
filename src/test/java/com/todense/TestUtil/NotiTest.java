09c253a9810a7f2af41e4708b31bcc288eb2526a
LtMarseille <philipp_berghoff@yahoo.com>
2024-08-01 16:54:01 +0200

    Use to Mok up the Notification center

---
+++ src/test/java/com/todense/TestUtil/NotiTest.java
@@ -0,0 +1,46 @@
+package com.todense.TestUtil;
+
+import de.saxsys.mvvmfx.utils.notifications.NotificationCenter;
+import de.saxsys.mvvmfx.utils.notifications.NotificationObserver;
+
+public class NotiTest implements NotificationCenter {
+    @Override
+    public void subscribe(String s, NotificationObserver notificationObserver) {
+
+    }
+
+    @Override
+    public void unsubscribe(String s, NotificationObserver notificationObserver) {
+
+    }
+
+    @Override
+    public void unsubscribe(NotificationObserver notificationObserver) {
+
+    }
+
+    @Override
+    public void publish(String s, Object... objects) {
+
+    }
+
+    @Override
+    public void publish(Object o, String s, Object[] objects) {
+
+    }
+
+    @Override
+    public void subscribe(Object o, String s, NotificationObserver notificationObserver) {
+
+    }
+
+    @Override
+    public void unsubscribe(Object o, String s, NotificationObserver notificationObserver) {
+
+    }
+
+    @Override
+    public void unsubscribe(Object o, NotificationObserver notificationObserver) {
+
+    }
+}
