e98888b4adf025f669314c3a8f26b3b8c50fb8db
Manuel Bürger <buerger-manuel@gmx.de>
2024-08-03 23:20:12 +0200

    Fix GraphColorerTest

--- src/test/java/com/todense/viewmodel/solver/graphColoring/GraphColorerTest.java
+++ src/test/java/com/todense/viewmodel/solver/graphColoring/GraphColorerTest.java
@@ -41,8 +41,8 @@ class GraphColorerTest {
         Graph result = GraphColorer.getColoredGraph(graph, ilp, jsonResponse);
         assertEquals("0x00ff00ff", result.getNodes().get(0).getColor().toString());
         assertEquals("0xffe502ff", result.getNodes().get(1).getColor().toString());
-        assertEquals("0xff00f6ff", result.getNodes().get(2).getColor().toString());
-        assertEquals("0xff00f6ff", result.getEdges().get(0).getColor().toString());
+        assertEquals("0xff0000ff", result.getNodes().get(2).getColor().toString());
+        assertEquals("0xff0000ff", result.getEdges().get(0).getColor().toString());
         assertEquals("0x00ff00ff", result.getEdges().get(1).getColor().toString());
         assertEquals("0xffe502ff", result.getEdges().get(2).getColor().toString());
     }
