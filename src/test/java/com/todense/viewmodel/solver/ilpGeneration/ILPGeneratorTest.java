313d81c6d255be6768649e65a8418586a549f1a8
Manuel Bürger <buerger-manuel@gmx.de>
2024-08-07 14:35:09 +0200

    Change ILPGenerationTest to test all TestCase json files

--- src/test/java/com/todense/viewmodel/solver/ilpGeneration/ILPGeneratorTest.java
+++ src/test/java/com/todense/viewmodel/solver/ilpGeneration/ILPGeneratorTest.java
@@ -1,35 +1,31 @@
 package com.todense.viewmodel.solver.ilpGeneration;
 
+import com.google.gson.Gson;
 import com.todense.model.graph.Edge;
 import com.todense.model.graph.Graph;
 import com.todense.model.graph.Node;
+import com.todense.viewmodel.file.format.ogr.OgrReader;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.ObjectInputFilter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ILPGeneratorTest {
 
-    @Test
-    void generateMinColorsILP() {
-        Graph graph = new Graph();
-        graph.addNode();
-        graph.addNode();
-        graph.addEdge(graph.getNodes().get(0), graph.getNodes().get(1));
-        ILPProblem ilp = ILPGenerator.generateILP(graph, ILPType.MINCOLORS);
-        String jsonString = ilp.getILPAsJsonString();
-        String expected = "{\"variables\":[\"x_v0_c0\",\"x_v0_c1\",\"x_v0_c2\",\"x_v1_c0\",\"x_v1_c1\"," +
-                "\"x_v1_c2\",\"y_e0_c0\",\"y_e0_c1\",\"y_e0_c2\",\"z_c0\",\"z_c1\",\"z_c2\"],\"constraints\"" +
-                ":[\"x_v0_c0 + x_v0_c1 + x_v0_c2 = 1\",\"x_v1_c0 + x_v1_c1 + x_v1_c2 = 1\"," +
-                "\"y_e0_c0 + y_e0_c1 + y_e0_c2 = 1\",\"x_v0_c0 + x_v1_c0 <= 1\",\"x_v0_c1 + x_v1_c1 <= 1\"," +
-                "\"x_v0_c2 + x_v1_c2 <= 1\",\"x_v0_c0 + y_e0_c0 <= 1\",\"x_v0_c1 + y_e0_c1 <= 1\"," +
-                "\"x_v0_c2 + y_e0_c2 <= 1\",\"x_v1_c0 + y_e0_c0 <= 1\",\"x_v1_c1 + y_e0_c1 <= 1\"," +
-                "\"x_v1_c2 + y_e0_c2 <= 1\",\"x_v0_c0 - z_c0 <= 0\",\"x_v0_c1 - z_c1 <= 0\",\"x_v0_c2 - z_c2 <= 0\"," +
-                "\"x_v1_c0 - z_c0 <= 0\",\"x_v1_c1 - z_c1 <= 0\",\"x_v1_c2 - z_c2 <= 0\",\"y_e0_c0 - z_c0 <= 0\"," +
-                "\"y_e0_c1 - z_c1 <= 0\",\"y_e0_c2 - z_c2 <= 0\"],\"optimizationFunction\":\"z_c0 + z_c1 + z_c2\"," +
-                "\"minimize\":true}";
-        assertEquals(expected, jsonString);
+    private class TestCase {
+        public boolean preferColor;
+        public String color;
+        public boolean tryKeepCurrentCollors;
+        public boolean forceCurrentColors;
+        public String testGraph;
+        public String expectedResult;
     }
+
 
     @Test
     void generateSetColorILP(){
@@ -152,5 +148,48 @@ class ILPGeneratorTest {
 
         ILPProblem ilp2 = ILPGenerator.generateILP(graph, ILPType.WITHSETCOLORS);
         assertNull(ilp2);
+    }
+
+    @Test
+    void testAllTestFiles(){
+        File testFolder = new File("src\\test\\resources\\ILPGenerationTests\\TestCases");
+        File[] testCases = testFolder.listFiles();
+        assertNotNull(testCases, "No test folder");
+        for (File file : testCases) {
+            if (file.isFile()) {
+                try {
+                    FileReader reader = new FileReader(file);
+                    Gson gson = new Gson();
+                    TestCase testCase = gson.fromJson(reader, TestCase.class);
+                    //find the ILP-Type
+                    boolean tryKeepCurrentCollors = testCase.tryKeepCurrentCollors;
+                    boolean preferColor = testCase.preferColor;
+                    boolean forceCurrentColors = testCase.forceCurrentColors;
+                    ILPType type = ILPType.MINCOLORS;
+                    if(tryKeepCurrentCollors){
+                        type = ILPType.SIMILARCOLORS;
+                    } else if(preferColor && !forceCurrentColors){
+                        type = ILPType.WITHLOWCOLORS;
+                        type.setMaximizeColor(Color.web(testCase.color));
+                    } else if(preferColor){
+                        type = ILPType.WITHLOWSETCOLORS;
+                        type.setMaximizeColor(Color.web(testCase.color));
+                    } else if(forceCurrentColors){
+                        type = ILPType.WITHSETCOLORS;
+                    }
+                    //get the test graph
+                    File graphFile = new File("src\\test\\resources\\ILPGenerationTests\\TestGraphs\\" +
+                            testCase.testGraph);
+                    OgrReader graphReader = new OgrReader();
+                    Graph testGraph = graphReader.readGraph(graphFile);
+                    ILPProblem ilpProblem = ILPGenerator.generateILP(testGraph, type);
+                    String result = ilpProblem.getILPAsJsonString();
+                    System.out.println(result);
+                    assertEquals(testCase.expectedResult, result);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
